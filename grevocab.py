b3ab39907bde43828b801503046f85b270c1e8c7
William Boyles <williamboyles22900@gmail.com>
2021-06-20 22:37:12 -0400

    Scrape to csv; column name change; query undefined

--- grevocab.py
+++ grevocab.py
@@ -1,3 +1,4 @@
+import csv
 import pickle
 from dataclasses import dataclass
 from random import randint, shuffle
@@ -15,7 +16,7 @@ class DefinedWord:
     """
     
     word:str
-    partOfSpeech:str
+    part_of_speech:str
     definition:str
     example_sentence:str
     synonyms:list[str]
@@ -42,7 +43,7 @@ def define_word(word:str, language:str="en_US") -> list[DefinedWord]:
     meanings = res['meanings']
 
     for meaning in meanings:
-        partOfSpeech = meaning['partOfSpeech']
+        part_of_speech = meaning['partOfSpeech']
 
         definitions = meaning['definitions']
         for definition in definitions:
@@ -50,23 +51,28 @@ def define_word(word:str, language:str="en_US") -> list[DefinedWord]:
             example_sentence = definition.get('example', "")
             synonyms = definition.get('synonyms', [])
 
-            defined_word = DefinedWord(word, partOfSpeech, adefinition, example_sentence, synonyms)
+            defined_word = DefinedWord(word, part_of_speech, adefinition, example_sentence, synonyms)
             wordList.append(defined_word)
     
     return wordList
 
-def scrape_words(outpath:str="words.pkl") -> list[str]:
+def scrape_words(outpath:str="words.csv"):
     """
     Scrape all the tables of GRE words on GraduateShotOnline.
-    Return the words in all of these tables in a single list.
+    Return thewords in all these tables in a single list.
+    Write the results to an output csv file.
     """
 
     # Number of tables of words GraduateShotOnline has
     num_tables = 5
 
-    words = []
+    # CSV writing items
+    outfile = open(outpath, 'w', encoding="utf-8", newline='')
+    writer = csv.writer(outfile)
 
-    # For each table, scrape just the word column, we'll get nicer defintions from the API
+    # Write the first row as column name
+    writer.writerow(["word"])
+
     for table in range(1, num_tables + 1):
         url = f"https://www.graduateshotline.com/gre/load.php?file=list{table}.html"
         res = requests.get(url)
@@ -75,31 +81,59 @@ def scrape_words(outpath:str="words.pkl") -> list[str]:
         table_rows = soup.find('table').find_all('tr')
         
         for row in table_rows:
-            word = row.find('td').find('a').text.lower()
-            words.append(word.split()[0])
+            word = row.find('td').find('a').text.split()[0].lower()
+            writer.writerow([word])
 
-    picklefile = open(outpath, 'wb')
-    pickle.dump(words, picklefile)
+    outfile.close()
 
-    return words
-
-def compile_words(outpath:str="words_dataframe.pkl") -> pd.DataFrame:
+def compile_words(wordspath:str="words.csv", outpath:str="words_dataframe.pkl") -> pd.DataFrame:
     """
-    Define all of the scraped words.
-    Put them together into a pandas dataframe.
+    Define all words in the wordspath file.
+    If the word is already defined in words_dataframe.pkl, we will not call the API again.
+    All defined words are returned as a dataframe and serialized to words_dataframe.pkl.
     """
 
-    words = []
-    for scraped_word in scrape_words():
-        for defined_word in define_word(scraped_word):
-            words.append(defined_word.__dict__)
+    # Open existing dataframe, if it exists
+    try:
+        with open(outpath, 'rb') as existing_words_file:
+            defined_words_df:pd.DataFrame = pickle.load(existing_words_file)
+    except FileNotFoundError:
+        defined_words_df = None
+
+    # Open csv file of words as dataframe
+    with open(wordspath) as wordsfile:
+        wordlist_df:pd.DataFrame = pd.read_csv(wordsfile)
     
-    words = pd.DataFrame(words)
+    # List of words that haven't been defined yet
+    defined_wordlist = []
 
-    picklefile = open(outpath, 'wb')
-    pickle.dump(words, picklefile)
+    for _, row in wordlist_df.iterrows():
+        # If we've already defined the word, no need to do it again
+        if defined_words_df is not None:
+            word_query = defined_words_df.query(f"word == '{row['word']}'")
 
-    return words
+            if word_query.empty:
+                continue
+
+        # Otherwise, define the word and append all its definitions
+        for defined_word in define_word(row['word']):
+           defined_wordlist.append(defined_word.__dict__)
+
+    # Dataframe of newly defined words
+    # It's less expensive to create new dataframe and add everything at once
+    newly_defined_word_df = pd.DataFrame(defined_wordlist)
+
+    # Combine already defined and newly defined words
+    if defined_words_df is None:
+        defined_words_df = newly_defined_word_df
+    else:
+        defined_words_df = defined_words_df.append(newly_defined_word_df)
+
+    # Serialize results
+    with open(outpath, 'wb') as picklefile:
+        pickle.dump(defined_words_df, picklefile)
+
+    return defined_words_df
 
 @dataclass
 class Question:
@@ -146,12 +180,10 @@ def random_question(num_options:int=5) -> Question:
     question.blank_sentence = rand_word['example_sentence'].replace(question.answer, "__________")
 
     # Get all the other words with the same part of speech as the answer
-    same_pos = words_df.query(f"partOfSpeech == '{rand_word['partOfSpeech']}' & word != '{rand_word['word']}'")
+    same_pos = words_df.query(f"part_of_speech == '{rand_word['part_of_speech']}' & word != '{rand_word['word']}'")
 
     # Generate num_options - 1 more incorrect answer choices
     question.choices = [same_pos.iloc[randint(0, len(same_pos) - 1)]['word'] for _ in range(1, num_options)]
     question.choices.append(question.answer)
 
     return question
-
-question = random_question()
