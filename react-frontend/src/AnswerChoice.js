5065ac28524c8ae388c194aede205221c0f2016a
William Boyles <williamboyles22900@gmail.com>
2021-06-26 01:40:11 -0400

    Show answer choices as buttons on screen
    Clicking them doesn't do anything yet...

---
+++ react-frontend/src/AnswerChoice.js
@@ -0,0 +1,5 @@
+function AnswerChoice(props) {
+  return <button class="btn btn-secondary btn-block">{props.choice}</button>;
+}
+
+export default AnswerChoice;
--- react-frontend/src/Question.js
+++ react-frontend/src/Question.js
@@ -1,9 +1,36 @@
+import { useEffect, useState } from "react";
+import AnswerChoice from "./AnswerChoice";
+
 function Question() {
+  // This might be kind of a hack, but it's the only way I found to prevent
+  // "Cannot read property 'map' of undefined" when trying to do data.choices.map.
+  const [data, setData] = useState({
+    blank_sentence: "",
+    choices: [],
+    answer: "",
+  });
+
+  useEffect(() => {
+    fetch("/question")
+      .then((res) => res.json())
+      .then((data) => {
+        setData(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
   return (
     <div class="jumbotron jumbotron-fluid">
       <div class="container">
-        /** TODO Get question, answer choices and format them. Will likely have
-        answer choice object that is repeated. */
+        <p class="lead">{data.blank_sentence}</p>
+        <hr class="my-4" />
+        <div class="container col-lg-10">
+          {data.choices.map((c) => (
+            <AnswerChoice key={c.id} choice={c} />
+          ))}
+        </div>
       </div>
     </div>
   );
