4ba98add8f136993b5e2a1a18ad23364bab871a6
William Boyles <williamboyles22900@gmail.com>
2021-06-26 20:12:00 -0400

    Prepare app for deployment

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 
 from flask_backend.api import api
 from flask_backend.view import view
@@ -11,6 +11,5 @@ app = Flask(
 app.register_blueprint(api, url_prefix="/api")
 app.register_blueprint(view)
 
-# This only gets run with doing py app.py, which we'd only do for debugging
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
